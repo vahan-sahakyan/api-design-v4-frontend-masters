36554e1ef82ef5b61bba3a24f6f74f8ead4338e5
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2023-06-22 14:51:40 +0400

    setting up protect mw

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -10,3 +10,26 @@ export const createJWT = (user) => {
   );
   return token;
 };
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+  if (!bearer) {
+    res.status(401).json({
+      message: "not authorized",
+    });
+    return;
+  }
+  const [, token] = bearer.split(" ");
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({
+      message: "not valid token",
+    });
+    return;
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express();
 
@@ -10,6 +11,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v4", router);
+app.use("/api/v4", protect, router);
 
 export default app;
